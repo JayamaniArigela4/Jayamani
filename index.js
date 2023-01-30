62f77a17cbb29b95ded4fb79a8b7ded01465b538
JayamaniArigela4 <123542778+JayamaniArigela4@users.noreply.github.com>
2023-01-30 11:38:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+import App1 from "./App1";
+import App2 from "./App2";
+ function Travel()
+ {
+  return(
+    <div>
+      <h1 style={{color:"blue"}}>Travel by bus or car</h1>
+      <App2></App2>
+      <App1></App1>
+    </div>
+  );
+ }
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(<Travel/>);
